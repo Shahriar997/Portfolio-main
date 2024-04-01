ad8c37d24a8075bed5c4dcd83c4c54b348d73ba6
Shahriar Hossain <66290875+Shahriar997@users.noreply.github.com>
2024-04-01 06:37:30 +0600

    innitial commit

---
+++ src/data/articles.js
@@ -0,0 +1,136 @@
+import React from "react";
+
+function article_1() {
+	return {
+		date: "2021",
+		title: "Education",
+		description:
+			"Completed My BSc in Computer Science from BRAC University in 2021",
+		keywords: [
+			"Shahriar Hossain",
+			"BRAC Univeristy"
+		],
+		style: `
+				.article-content {
+					display: flex;
+					flex-direction: column;
+					align-items: center;
+				}
+
+				.randImage {
+					align-self: center;
+					outline: 2px solid red;
+				}
+				`,
+		body: (
+			<React.Fragment>
+				<div className="article-content">
+					<h1>BRAC Univeristy</h1>
+					<div className="paragraph">Bachelors in Computer Science, 2018-2021</div>
+					<div className="paragraph">CGPA: 3.72</div>
+					
+				</div>
+				<div className="article-content">
+					<h1>|</h1>
+				</div>
+				<div className="article-content">
+					<h1>Dhaka City College</h1>
+					<div className="paragraph">Higher Secondary Certificate (HSC), 2017</div>
+					<div className="paragraph">Science, GPA: 5.00</div>
+				</div>
+			</React.Fragment>
+		),
+	};
+}
+
+function article_2() {
+	return {
+		date: "January 09, 2022 - Present",
+		title: "Junior Software Engineer at Orbit Informatics",
+		description:
+			"I started working at Orbit Informatics right after my graduation. Currently I am working as a Junior Software Engineer. During this time period with this company, I have contributed on their several projects. For Example, Aquila PQMS, Prepmock, Oceandocs, VoucherMe and JOE.",
+		keywords: [
+			"Shahriar Hossain",
+			"Orbit Informatics",
+			"Junior Software Engineer",
+			"Jr Software Engineer",
+			"Jr. Software Engineer",
+		],
+		style: `
+				.article-content {
+					display: flex;
+					flex-direction: column;
+					align-items: start;
+				}
+				`,
+		body: (
+			<React.Fragment>
+				<div className="article-content">
+					<li className="paragraph">During my tenure at Orbit Informatics, I thrived in an agile environment, actively contributing to various projects primarily centered around PHP, each utilizing different frameworks. Additionally, I provided support for React applications, facilitating modifications to existing projects.</li>
+					<li className="paragraph">Used project management tools such as, Jira & Version Control tools such as, Git & Github. </li>
+					<li className="paragraph">Worked with cloud, third party services & API. Such as, MangoPay Payment Gateway SendGrid and MailGun DocRaptor, CloudConvert. </li>
+					<li className="paragraph">Have Experience With AWS EC2, ASG, SQS, Scheduler etc. </li>
+					<li className="paragraph">Projects I have Worked On: Aquila PQMS, Prepmock, JOE, OceadDocs, VoucherMe, Jambura a Custom Framework, etc. </li>
+				</div>
+			</React.Fragment>
+		),
+	};
+}
+
+function article_3() {
+	return {
+		date: "N/A",
+		title: "Certifications",
+		description:
+			"Learning new technologies always excites me. I do search for courses with certifications and try to obtain that for my further growth. I have completed some courses and currently doing one.",
+		keywords: [
+			"Shahriar Hossain",
+			"UDEMY",
+			"MERN",
+			"Laravel"
+		],
+		style: `
+				.article-content {
+					display: flex;
+					flex-direction: column;
+					align-items: start;
+				}
+
+				`,
+		body: (
+			<React.Fragment>
+				<div className="article-content">
+					<li className="paragraph">React, NodeJS, Express & MongoDB - The MERN Fullstack Guide - <a href={"https://www.udemy.com/certificate/UC-3e62f773-b0d6-411b-b0bf-2024b731e385/"}>Certificate</a> </li>
+					<li className="paragraph">Master Laravel for Beginners & Intermediate 2024 - Currently Doing - <a href={"https://www.udemy.com/course/laravel-beginner-fundamentals/?couponCode=ACCAGE0923"}>Course Link	</a> </li>
+					<li className="paragraph">Spring Boot For Beginners - <a href={"../spring-boot-cert.pdf"}>Certificate</a> </li>
+					<li className="paragraph">More To Come ... </li>
+				</div>
+			</React.Fragment>
+		),
+	};
+}
+
+// function article_2() {
+// 	return {
+// 		date: "7 May 2023",
+// 		title: "Artificial Intelligence in Healthcare",
+// 		description:
+// 			"AI is transforming the healthcare industry, from improving patient outcomes to streamlining operations. Discover the latest applications of this game-changing technology.",
+// 		style: ``,
+// 		keywords: [
+// 			"Artificial Intelligence in Healthcare",
+// 			"Tharindu",
+// 			"Tharindu N",
+// 			"Tharindu Nayanajith",
+// 		],
+// 		body: (
+// 			<React.Fragment>
+// 				<h1>Content of article 2</h1>
+// 			</React.Fragment>
+// 		),
+// 	};
+// }
+
+const myArticles = [article_1, article_2, article_3];
+
+export default myArticles;
---
+++ src/data/user.js
@@ -0,0 +1,82 @@
+const INFO = {
+	main: {
+		title: "Shahriar Hossain Portfolio",
+		name: "Shahriar Hossain",
+		email: "shahriar.hossain997@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/Shahriar997",
+		linkedin: "https://www.linkedin.com/in/shahriar-hossain997/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Software Engineer | PHP, JS, JAVA",
+		description: `As a backend developer, I specialize in PHP and Node.js, leveraging my expertise to construct scalable, secure, and dependable web applications. 
+		My proficiency extends across multiple frameworks and technologies, including Laravel, Yii, custom PHP frameworks, and Express.js. 
+		Committed to continuous learning, I eagerly embrace new skills and languages. 
+		My dedication to crafting high-caliber code adhering to industry standards drives my passion for excellence. 
+		I thrive on tackling fresh challenges and seizing opportunities for professional growth within the development sphere.`,
+	},
+
+	about: {
+		title: "I’m Shahriar Hossain from Dhaka, Bangladesh.",
+		description: `Energetic individual, enthusiastic towards technology. 
+		Completed my major in Computer Science at BRAC University and currently working as a Jr. Software Engineer. 
+		Gaining proficiency in Web development and experience of working with agile methodology. 
+		Can communicate with others confidently and effectively as well as hold the vision, analytical ability and fairness to lead a team. 
+		Organized and driven with the innate ability to stay on task and work under pressure. 
+		I believe in Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.`,
+	},
+
+	articles: {
+		title: "Education, Professional Experience and Attained Certifications",
+		description: "I graduated with a Bachelor of Science in Computer Science from BRAC University in 2021 and commenced my professional journey in January 2022 as a Junior Software Engineer. Concurrently, I am engaged in various side projects and actively pursuing further education in diverse technologies through Udemy.",
+	},
+
+	projects: [{
+			title: "Tawaki Finance Buddy",
+			description: " Project for a local import company ‘Tawaki Power Generation’. With the motivation of keeping all financial Transactions transparent among the partners. It tracks and calculate all financial records. The MVP (most viable product) was sent to production within a month.",
+			logo: "https://www.cdnlogo.com/logos/l/57/laravel.svg",
+			linkText: "View Project",
+			link: "https://github.com/Shahriar997",
+		},
+
+		{
+			title: "Your Places - Share Favorite Places",
+			description: "Here, a user can share places they have visited. Can rate them. Add google map pin point. And others can view it. This project was made while learning MERN stack course from UDEMY.",
+			logo: "../mern.png",
+			linkText: "View Project",
+			link: "https://github.com/Shahriar997/Your-Places--Share-and-rate-your-favorite-places-Backend-ExpressJS-and-Mongo",
+		},
+
+		{
+			title: "JOE",
+			description: "With the intention of saving AWS costings for big projects, JOE was built to make a server sleep when it is unused. With JOE, even a non-technical person can shutdown an ec2 instance, RDS and modify ASG as well. This is a project of my current company and I have a big contribution in it.",
+			logo: "https://www.cdnlogo.com/logos/p/71/php.svg",
+			linkText: "Project Staging Url",
+			link: "https://joe-stg.prepmock.com/signin",
+		},
+
+		{
+			title: "Aquila PQMS",
+			description: "ne of the core and technically critical project of my current company which is a secure cloudbased communication and document management application for ship building projects.  This is a project of my current company and I have a big contribution in it as well.",
+			logo: "https://www.cdnlogo.com/logos/y/34/yii.svg",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "VoucherMe",
+			description: "A project of Ireland where they sell gift vouchers of different brands.  This is a project of my current company and I have a big contribution in it as well.",
+			logo: "https://www.cdnlogo.com/logos/c/31/codeigniter.svg",
+			linkText: "Project Live Url",
+			link: "https://www.voucherme.ie/",
+		},
+	],
+};
+
+export default INFO;
